7518255b1d9ed9617d768af545187dbba88af1e2
Senstur Games <senstur.art@gmail.com>
2023-10-13 10:00:33 -0300

    0.1

---
+++ Project/Level/Level.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _ready():
+	var d = GameData.GetLevelData("10")
+	pass 
+
+func _process(delta):
+	pass
---
+++ Singletons/GameData.gd
@@ -0,0 +1,17 @@
+extends Node
+
+const LEVEL_DATA_PATH: String = "res://Data/LevelData.json"
+const TILE_SIZE: int = 32
+
+var leveldata: Dictionary = {}
+
+func _ready():
+	LoadLevelData()
+
+func LoadLevelData():
+	var file: = FileAccess.open(LEVEL_DATA_PATH,FileAccess.READ)
+	leveldata = JSON.parse_string(file.get_as_text())
+	pass
+
+func GetLevelData(level: String) -> Dictionary:
+	return leveldata[level]
