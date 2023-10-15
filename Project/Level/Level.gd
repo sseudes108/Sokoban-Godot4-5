065ea7034f450b7e74fb8647829b4c88ae584311
Senstur Games <senstur.art@gmail.com>
2023-10-14 22:26:46 -0300

    0.4

--- Project/Level/Level.gd
+++ Project/Level/Level.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var tileMap = $TileMap
-@onready var player = $Player
+@onready var player = $Finn
 @onready var camera = $Camera2D
 
 const FLOOR_LAYER = 0
@@ -25,11 +25,107 @@ const LAYER_MAP = {
 	LAYER_KEY_BOXES: BOX_LAYER
 }
 
+var isMoving: bool = false
+var movesMade: int = 0
+
 func _ready():
-	SetUpLevel() 
+	SetUpLevel()
 
 func _process(delta):
-	pass
+	
+	if isMoving == true:
+		return
+	
+	var direction = Vector2i.ZERO
+	
+	if Input.is_action_just_pressed("right"):
+		player.flip_h = false
+		direction = Vector2i.RIGHT
+	if Input.is_action_just_pressed("left"):
+		player.flip_h = true
+		direction = Vector2i.LEFT
+	if Input.is_action_just_pressed("up"):
+		direction = Vector2i.UP
+	if Input.is_action_just_pressed("down"):
+		direction = Vector2i.DOWN
+	
+	if Input.is_action_just_pressed("reload"):
+		movesMade = 0
+		SetUpLevel()
+	
+	if direction != Vector2i.ZERO:
+		MovePlayer(direction)
+
+############
+#Game Logic#
+############
+
+func GetPlayerCurrentTile() -> Vector2i:
+	var playerOffset = player.global_position - tileMap.global_position
+	return Vector2i(playerOffset / GameData.TILE_SIZE)
+
+## Check cell the player is trying to move to
+
+func MoveBox(boxTile: Vector2i, direction: Vector2i):
+	var dest = boxTile + direction
+	
+	tileMap.erase_cell(BOX_LAYER, boxTile)
+	
+	if dest in tileMap.get_used_cells(TARGET_LAYER):
+		tileMap.set_cell(BOX_LAYER,dest,SOURCE_ID,
+		GetAtlasCordForLayerName(LAYER_KEY_TARGET_BOXES))
+	else:
+		tileMap.set_cell(BOX_LAYER,dest,SOURCE_ID, 
+		GetAtlasCordForLayerName(LAYER_KEY_BOXES))
+
+func cellIsWall(cell: Vector2i) -> bool:
+	return cell in tileMap.get_used_cells(WALL_LAYER)
+
+func cellIsBox(cell: Vector2i) -> bool:
+	return cell in tileMap.get_used_cells(BOX_LAYER)
+
+func cellIsEmpty(cell: Vector2i) -> bool:
+	return cellIsWall(cell) == false and cellIsBox(cell) == false
+
+func BoxCanMove(boxTile: Vector2i, direction: Vector2i) -> bool:
+	var newBoxTile = boxTile + direction
+	return cellIsEmpty(newBoxTile)
+
+func MovePlayer(direction: Vector2i):
+	isMoving = true
+	
+	var playerTile = GetPlayerCurrentTile()
+	var newPlayerTile = playerTile + direction
+	var canMove = true
+	var boxSeen = false
+	
+	if cellIsWall(newPlayerTile):
+		canMove = false
+	if cellIsBox(newPlayerTile):
+		boxSeen = true
+		canMove = BoxCanMove(newPlayerTile, direction)
+	
+	if canMove:
+		print("Can Move")
+		movesMade +=1
+		print("Moves: ", movesMade)
+		if boxSeen:
+			MoveBox(newPlayerTile,direction)
+		
+		SetPlayerOnTile(newPlayerTile);
+	
+	isMoving = false
+
+
+#######################################
+# TileMap, camera and player placment #
+#######################################
+func SetPlayerOnTile(tileCoord: Vector2i):
+	var newPos: Vector2 = Vector2(
+		tileCoord.x * GameData.TILE_SIZE,
+		tileCoord.y * GameData.TILE_SIZE,
+	) + tileMap.global_position
+	player.global_position = newPos
 
 func GetAtlasCordForLayerName(layerName: String) -> Vector2i:
 	match layerName:
@@ -58,26 +154,30 @@ func AddLayerTiles(layerTiles, layerName: String):
 
 func SetUpLevel():
 	tileMap.clear()
-	var levelData = GameData.GetLevelData("12")
+	var levelData = GameData.GetLevelData("6")
 	var levelTiles = levelData.tiles
 	var playerStart = levelData.player_start
 	
 	for layerName in LAYER_MAP.keys():
 		AddLayerTiles(levelTiles[layerName], layerName)
+	
+	SetPlayerOnTile(Vector2i(playerStart.x,playerStart.y))
+	MoveCamera()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+func MoveCamera():
+	#Used tile rectangle
+	var tileRectangle = tileMap.get_used_rect()
+	
+	var tileRecStartX = tileRectangle.position.x * GameData.TILE_SIZE
+	var tileRecEndX = tileRectangle.size.x * GameData.TILE_SIZE + tileRecStartX
+	
+	var tileRecStartY = tileRectangle.position.y * GameData.TILE_SIZE
+	var tileRecEndY = tileRectangle.size.y * GameData.TILE_SIZE + tileRecStartY
+	
+	var midX = tileRecStartX + (tileRecEndX - tileRecStartX) / 2
+	var midY = tileRecStartY + (tileRecEndY - tileRecStartY) / 2
+	
+	camera.position = Vector2(midX, midY)
 
 
 
